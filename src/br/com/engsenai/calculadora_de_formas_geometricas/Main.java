1dceb381fa5b0060513adad31ce1da9778233c69
ISABELLE DOS SANTOS DE ABREU <25132976@dsn127m1242315.cfp127aluno.local>
2025-03-19 17:01:02 -0300

    criado classe trapezio

--- src/br/com/engsenai/calculadora_de_formas_geometricas/Main.java
+++ src/br/com/engsenai/calculadora_de_formas_geometricas/Main.java
@@ -1,14 +1,12 @@
 package br.com.engsenai.calculadora_de_formas_geometricas;
 
-import br.com.engsenai.calculadora_de_formas_geometricas.model.Quadrado;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-				Quadrado q1 = new Quadrado();
-				q1.setLado(5);
-				q1.mostrarDados();
+				
 	}
 
 }
---
+++ src/br/com/engsenai/calculadora_de_formas_geometricas/model/Trapezio.java
@@ -0,0 +1,48 @@
+package br.com.engsenai.calculadora_de_formas_geometricas.model;
+
+public class Trapezio {
+	
+	private double baseMaior;
+	private double baseMenor;
+	private double altura;
+	
+	public void setBaseMaior(double baseMaior) {
+		this.baseMaior = baseMaior;
+	}
+	
+	public double getBaseMaior () {
+		return this.baseMaior;
+	}
+
+	public double getBaseMenor() {
+		return baseMenor;
+	}
+
+	public void setBaseMenor(double baseMenor) {
+		this.baseMenor = baseMenor;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	private double calcularArea() {
+		double area = (baseMaior + baseMenor) * altura / 2;
+		return area;
+	}
+	
+	public void mostrarDados() {
+		System.out.println("-------------------");
+		System.out.println("T R A P É Z I O");
+		System.out.println("-------------------");
+		System.out.println("Base Menor: " + baseMenor);
+		System.out.println("Base Maior: " + baseMaior);
+		System.out.println("Altura: " + altura);
+		System.out.println("Área: " + calcularArea;());
+		System.out.println("-------------------");
+	}
+}
